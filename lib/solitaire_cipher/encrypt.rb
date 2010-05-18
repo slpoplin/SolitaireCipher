06dbcd83fbcb46872507b046e6df9f235a37e5c8
Spencer <spencer@spencer-linux-desk.(none)>
2010-05-17 22:24:06 -0400

    Passed easy test.  Added script to run program.

--- lib/solitaire_cipher/encrypt.rb
+++ lib/solitaire_cipher/encrypt.rb
@@ -1,11 +1,11 @@
 module SolitaireCipher
 	class Encrypt
 		def initialize(output)
-			
+			@output = output
 		end
 	
 		def run
-			
+			@output.puts 'Enter text to encrypt:'
 		end
 	end
 end
--- spec/solitaire_cipher/encrypt_spec.rb
+++ spec/solitaire_cipher/encrypt_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 module SolitaireCipher
 	describe Encrypt do
 		describe "#run" do
-			
+			it "prompts for text" do
+				output = double('output').as_null_object
+				encrypt = Encrypt.new(output)
+				output.should_receive(:puts).with('Enter text to encrypt:')
+				encrypt.run
+			end
 		end
 	end
 end
