54c70d1fd4cf9d0609117d4e894b62af8ce5f861
Spencer <spencer@spencer-linux-desk.(none)>
2010-06-09 22:58:02 -0400

    Got the rspec stuff working by adding the proper requires and includes to spec_helper.

--- spec/solitaire_cipher/encrypt_spec.rb
+++ spec/solitaire_cipher/encrypt_spec.rb
@@ -4,7 +4,8 @@ module SolitaireCipher
 	describe Encrypt do
 		describe "#run" do
 			it "outputs the cipher"
-				output = double('output')
+				output = Mock.new('output')
+				#output = Spec.Mocks.Mock.new('output')
 				encrypt = Encrypt.new(output)
 				
 				output.should_receive(:puts).with('GLNCQ MJAFF FVOMB JIYCB')
@@ -12,24 +13,3 @@ module SolitaireCipher
 		end
 	end
 end
-
-
-
-#module Codebreaker
-#  describe Encrypt do
-#    describe "#run" do
-#      let(:output) { double('output').as_null_object }
-#      let(:encrypt)   { Encrypt.new(output) }
-#      
-#      it "sends a welcome message" do
-#        output.should_receive(:puts).with('Welcome to Codebreaker!')
-#        game.start('1234')
-#      end
-
-#      it "prompts for the first guess" do
-#        output.should_receive(:puts).with('Enter guess:')
-#        game.start('1234')
-#      end
-#    end
-#  end
-#end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 require 'solitaire_cipher'
+
+require 'spec'
+require 'spec/mocks'
+require 'spec/mocks/mock.rb'
+
+include Spec
+include Spec::Mocks
+include Spec::Mocks::ExampleMethods
